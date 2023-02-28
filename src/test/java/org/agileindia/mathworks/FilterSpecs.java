aa2f0b86c494ed4252c1fb7c5247ad60ff8e03c2
Bhavesh Furia <bhaveshfuria@Bhaveshs-MacBook-Pro.local>
2023-02-28 13:21:11 +0530

    Day 2 - Refactored duplicate code

--- src/test/java/org/agileindia/mathworks/FilterSpecs.java
+++ src/test/java/org/agileindia/mathworks/FilterSpecs.java
@@ -1,6 +1,5 @@
 package org.agileindia.mathworks;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -34,5 +33,44 @@ public class FilterSpecs {
         //Then
         assertThat(primeNumbers, hasSize(1));
         assertThat(primeNumbers, hasItem(6));
+    }
+
+    private void processAndAssertFilterEven(List<Integer> numbers, int expectedSize, List<Integer> expectedItems){
+
+        //When
+        List<Integer> evenNumbers = Filter.selectEven(numbers);
+
+        //Then
+        assertThat(evenNumbers, hasSize(expectedSize));
+        for(Integer expectedNum : expectedItems){
+            assertThat(evenNumbers, hasItem(expectedNum));
+        }
+    }
+
+    @Test
+    public void filterEvenNumbersFromPositiveNumInputs(){
+        processAndAssertFilterEven(Arrays.asList(1, 2, 3, 4), 2, Arrays.asList(2,4));
+    }
+
+    @Test
+    public void filterEvenNumbersFromMixNumInputs(){
+        processAndAssertFilterEven(Arrays.asList(1, 2, -4), 2, Arrays.asList(2,-4));
+    }
+
+    @Test
+    public void shouldBeAbletoGetEvenPerfectNumbers(){
+        processAndAssertFilterEvenAndPerfect(Arrays.asList(1,2,3,6,8,27,28), 2, Arrays.asList(6,28));
+    }
+
+    private void processAndAssertFilterEvenAndPerfect(List<Integer> numbers, int expectedSize, List<Integer> expectedItems){
+
+        //When
+        List<Integer> evenPerfectNumbers = Filter.selectEvenPerfect(numbers);
+
+        //Then
+        assertThat(evenPerfectNumbers, hasSize(expectedSize));
+        for(Integer expectedNum : expectedItems){
+            assertThat(evenPerfectNumbers, hasItem(expectedNum));
+        }
     }
 }
