ede0fcc61328b3fa4109110c49df2d21be64b837
Bhavesh Furia <furia.bhavesh@gmail.com>
2023-03-01 12:39:21 +0530

    Day 3 (#2)
    * Day 2 - Refactored duplicate code
    
    * replaced predicates with constants
    
    * adding support for multiple predicates
    
    * pass filter predicate from client instead of calling exploded method, deleted unrequired methods
    
    * filter numbers based on multiple predicates
    
    * [refactor][wip] - identify paragraph in code and moved to compose method
    
    * [refactor] - sum of numbers moved to a separate method
    
    * [refactor] - inlined variables for better readability - literate programming :)
    
    * inverted if condition to make a guard clause
    
    * changed function order such that public is at top and private at bottom
    
    ---------
    
    Co-authored-by: Bhavesh Furia <bhaveshfuria@Bhaveshs-MacBook-Pro.local>

--- src/main/java/org/agileindia/mathworks/Filter.java
+++ src/main/java/org/agileindia/mathworks/Filter.java
@@ -5,51 +5,52 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Filter {
-    public static List<Integer> selectPerfect(List<Integer> numbers) {
-        Predicate<Integer> predicate = Filter::isPerfect;
-        return select(numbers, predicate);
+
+    public static final Predicate<Integer> EVEN = Filter::isEven;
+    public static final Predicate<Integer> PERFECT = Filter::isPerfect;
+
+    @SafeVarargs
+    public static List<Integer> select(final List<Integer> numbers, Predicate<Integer> ... predicates) {
+        List<Integer> filteredNumbers = numbers;
+        List<Integer> temp;
+        for (Predicate<Integer> predicate: predicates){
+            temp = new ArrayList<>();
+            for (Integer number : filteredNumbers) {
+                if (predicate.test(number)) {
+                    temp.add(number);
+                }
+            }
+            filteredNumbers = temp;
+        }
+        return filteredNumbers;
     }
 
     private static boolean isPerfect(int number) {
-        if (number > 0) {
-            List<Integer> factors = new ArrayList<>();
-            for (int i = 1; i <= number; i++) {
-                if (number % i == 0) {
-                    factors.add(i);
-                }
-            }
-            // Sum of factors
-            int sumOfFactors = 0;
-            for (Integer i : factors) {
-                sumOfFactors += i;
-            }
-            // It is a perfect number if the difference between sum of factors and the
-            // number is equal to the number itself
-            return sumOfFactors - number == number;
-        }
-        return false;
-    }
+        if (number <= 0)
+            return false;
 
-    public static List<Integer> selectEven(List<Integer> numbers) {
-        Predicate<Integer> predicate = Filter::isEven;
-        return select(numbers, predicate);
+        return sum(factors(number)) - number == number;
     }
 
     private static boolean isEven(Integer number) {
         return number % 2 == 0;
     }
 
-    public static List<Integer> select(List<Integer> numbers, Predicate<Integer> predicate) {
-        List<Integer> filteredNumbers = new ArrayList<>();
+    private static int sum(List<Integer> numbers) {
+        int sum = 0;
         for (Integer number : numbers) {
-            if(predicate.test(number)){
-                filteredNumbers.add(number);
-            }
+            sum += number;
         }
-        return filteredNumbers;
+        return sum;
     }
 
-    public static List<Integer> selectEvenPerfect(List<Integer> numbers) {
-        return select(select(numbers, Filter::isPerfect), Filter::isEven);
+    private static List<Integer> factors(int number) {
+        List<Integer> factors = new ArrayList<>();
+        for (int divisor = 1; divisor <= number; divisor++) {
+            if (number % divisor == 0) {
+                factors.add(divisor);
+            }
+        }
+        return factors;
     }
 }
